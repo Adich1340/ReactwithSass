358b080214ba06238181f8d3431d004befdd37d7
Adich1340 <adich1340@gmail.com>
2020-03-30 19:50:22 +0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import './App.scss';
+
+import CountUp from 'react-countup';
+
+
 
 function App() {
+  const [confirmed, setConfirmed] = useState(0)
+  const [recovered, setRecovered] = useState(0)
+  const [deaths, setDeaths] = useState(0)
+
+
+    useEffect(() => {
+      async function fetchData() {
+        const data = await fetch("https://covid19.mathdro.id/api/countries/IL")
+        .then(res => res.json());
+        setConfirmed(data.confirmed.value)
+        setRecovered(data.recovered.value)
+        setDeaths(data.deaths.value)
+      }
+      fetchData();
+    }, [confirmed, recovered, deaths])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="title">
+        <h2>CoronaVirus Data</h2>
+      </div>
+      <div className="dataNumbers">
+        <span>
+          <CountUp className="item" start={0} end={confirmed} duration={2.75} useEasing={true}/>
+        </span>
+        <span>
+          <CountUp className="item second" start={0} end={recovered} />
+        </span>
+        <span>
+        <CountUp className="item third" start={0} end={deaths} />
+        </span>
+      </div>
+   </div>
   );
 }
-
 export default App;
