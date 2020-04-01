208773e57724e041e4d33718b0ab6e98c27cb888
Adich1340 <adich1340@gmail.com>
2020-04-01 14:08:11 +0300

    Added countris option and their data to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import './App.scss';
 
@@ -10,36 +10,70 @@ function App() {
   const [confirmed, setConfirmed] = useState(0)
   const [recovered, setRecovered] = useState(0)
   const [deaths, setDeaths] = useState(0)
-
+  const [country, setCountry] = useState([])
+  const numberOfIsraelCalls = useRef(0);
+  const numberOfCountriesCalls = useRef(0);
 
     useEffect(() => {
       async function fetchData() {
-        const data = await fetch("https://covid19.mathdro.id/api/countries/IL")
+        const dataCountries = await fetch("https://covid19.mathdro.id/api/countries")
         .then(res => res.json());
-        setConfirmed(data.confirmed.value)
-        setRecovered(data.recovered.value)
-        setDeaths(data.deaths.value)
+        const newCountry = Object.keys(dataCountries.countries)
+        .map((i => dataCountries.countries[i].name));
+        setCountry(newCountry)
+        console.log("Number of countries renders: " + numberOfCountriesCalls.current++);
+
       }
       fetchData();
-    }, [confirmed, recovered, deaths])
+    }, [])
+
+  function renderCountryOption(){
+    return country.map((place, i) =>{
+      return <option key={i}>{place}</option>
+    })
+  }
+
+  async function getCountryData(e){
+    try{
+      const countryData = await fetch(`https://covid19.mathdro.id/api/countries/${e.target.value}`)
+        .then(res => res.json());
+        console.log(countryData.confirmed.value)
+        setConfirmed(countryData.confirmed.value)
+        setRecovered(countryData.recovered.value)
+        setDeaths(countryData.deaths.value)
+      }
+    catch (err) {
+
+      }
+  }
+
 
   return (
     <div className="container">
-      <div className="title">
+      <div className="header">
         <h2>CoronaVirus Data</h2>
       </div>
+      <div>
+        <select className="dropdown" onChange={getCountryData} >
+        {renderCountryOption()}
+        </select>
+      </div>
       <div className="dataNumbers">
-        <span>
-          <CountUp className="item" start={0} end={confirmed} duration={2.75} useEasing={true}/>
+        <span className="item">
+          <h3 className="title">Confirmed</h3>
+          <CountUp start={0} end={confirmed} duration={2.75} useEasing={true}/>
         </span>
-        <span>
-          <CountUp className="item second" start={0} end={recovered} />
+        <span className="item second">
+          <h3 className="title">Recovered</h3>
+          <CountUp start={0} end={recovered} />
         </span>
-        <span>
-        <CountUp className="item third" start={0} end={deaths} />
+        <span className="item third">
+        <h3 className="title">Deaths</h3>
+        <CountUp start={0} end={deaths} />
         </span>
       </div>
    </div>
   );
 }
+
 export default App;
